c687264c1b70f4bf53665543a034bb38f5e41ef4
amigin <33rkjgerhjg51>
2019-01-04 02:54:03 +0200

    Handle static file for frontend project being hosted by the same project

--- publicationplatform.api/Startup.cs
+++ publicationplatform.api/Startup.cs
@@ -46,6 +46,8 @@ namespace PublicationPlatform.api
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                // app.UseHsts();
             }
+            
+            app.UseStaticFiles();
 
             //app.UseHttpsRedirection();
             app.UseMvc();
